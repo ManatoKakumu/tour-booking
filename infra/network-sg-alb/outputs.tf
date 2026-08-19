92c5937b3461dc8a25d2bca63271238635919541
ManatoKakumu <tmnwbsabcdc@docomo.ne.jp>
2026-08-19 18:05:14 +0900

    add: oidc追加

--- infra/network-sg-alb/outputs.tf
+++ infra/network-sg-alb/outputs.tf
@@ -5,3 +5,7 @@ output "db_subnet_ids" {
 output "rds_security_group_id" {
   value = aws_security_group.rds.id
 }
+
+output "iam_openid_connect_provider" {
+  value = aws_iam_openid_connect_provider.github_actions.arn
+}
--- infra/network-sg-alb/provider.tf
+++ infra/network-sg-alb/provider.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.24"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
