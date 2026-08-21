171322959a3a04a6d7bc6b96f978e0b359643ec1
ManatoKakumu <tmnwbsabcdc@docomo.ne.jp>
2026-08-21 11:35:25 +0900

    add: ECSサービスの追加

--- infra/compute-b/ecs.tf
+++ infra/compute-b/ecs.tf
@@ -17,12 +17,18 @@ locals {
       ecr_repository_url = aws_ecr_repository.front_b.repository_url
       log_group_name     = aws_cloudwatch_log_group.front_b.name
       image_tag          = var.front_b_image_tag
+      subnet_ids         = data.terraform_remote_state.network_sg_alb.outputs.ecs_front_subnet_ids
+      security_group_id  = data.terraform_remote_state.network_sg_alb.outputs.ecs_front_security_group_id
+      target_group_arn   = data.terraform_remote_state.network_sg_alb.outputs.target_group_arns["front-b"]
     }
     api-b = {
       execution_role_arn = aws_iam_role.ecs_execution_api_b.arn
       ecr_repository_url = aws_ecr_repository.api_b.repository_url
       log_group_name     = aws_cloudwatch_log_group.api_b.name
       image_tag          = var.api_b_image_tag
+      subnet_ids         = data.terraform_remote_state.network_sg_alb.outputs.ecs_api_subnet_ids
+      security_group_id  = data.terraform_remote_state.network_sg_alb.outputs.ecs_api_security_group_id
+      target_group_arn   = data.terraform_remote_state.network_sg_alb.outputs.target_group_arns["api-b"]
     }
   }
 }
@@ -56,5 +62,26 @@ resource "aws_ecs_task_definition" "this" {
 
   lifecycle {
     ignore_changes = [container_definitions]
+  }
+}
+
+resource "aws_ecs_service" "this" {
+  for_each        = local.ecs_services
+  name            = each.key
+  cluster         = aws_ecs_cluster.b.id
+  task_definition = aws_ecs_task_definition.this[each.key].arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = each.value.subnet_ids
+    security_groups  = [each.value.security_group_id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = each.value.target_group_arn
+    container_name   = each.key
+    container_port   = 3000
   }
 }
