ab265454b2bbfcbdd6b86a625c4603d3baac93b1
ManatoKakumu <tmnwbsabcdc@docomo.ne.jp>
2026-08-29 18:38:26 +0900

    add: ホストゾーンの追加

--- infra/cloudfront-route53-acm-s3/acm.tf
+++ infra/cloudfront-route53-acm-s3/acm.tf
@@ -16,3 +16,30 @@ resource "aws_acm_certificate" "alb" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "cloudfront_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cloudfront.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 300
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "cloudfront" {
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.cloudfront.arn
+  validation_record_fqdns = [for record in aws_route53_record.cloudfront_cert_validation : record.fqdn]
+}
+
+resource "aws_acm_certificate_validation" "alb" {
+  certificate_arn         = aws_acm_certificate.alb.arn
+  validation_record_fqdns = [for record in aws_route53_record.cloudfront_cert_validation : record.fqdn]
+}
