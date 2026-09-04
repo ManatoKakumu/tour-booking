618bfcafff4a2353798b20a5527099a33e15cf2a
ManatoKakumu <tmnwbsabcdc@docomo.ne.jp>
2026-09-04 18:13:20 +0900

    add: Budgets追加

---
+++ infra/cost-management/budgets.tf
@@ -0,0 +1,44 @@
+variable "notification_email" {
+  type      = string
+  sensitive = true
+}
+
+resource "aws_budgets_budget" "main" {
+  name         = "tour-booking-budget"
+  budget_type  = "COST"
+  limit_amount = "18"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 50
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.notification_email]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.notification_email]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = [var.notification_email]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = [var.notification_email]
+  }
+}
